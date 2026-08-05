d56d059f2a9b2a98a98953fdd9b76557acea6869
AKASH SHUKLA <akashshukla94097@gmail.com>
2026-08-05 16:34:47 +0000

    conversion

---
+++ basic/conversion.js
@@ -0,0 +1,22 @@
+// let number=33;
+// console.log(typeof number)
+// console.log(typeof (number))
+// let number="33";
+// console.log(typeof number)
+// let number="33ah";
+// let integer=Number(number)
+// console.log(typeof integer)
+// console.log( integer)
+// let bool=1;
+// let bollean=Boolean(bool)
+// console.log(bollean)
+let bool="";//if string empty then false if anything in this string then true
+let bollean=Boolean(bool)
+console.log(bollean)
+let num=20
+let st=String(num)
+console.log(typeof(st))
+
+
+
+
