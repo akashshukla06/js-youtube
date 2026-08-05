8080d4915dcd43199731faf19cbf8e49e6b6bee2
AKASH SHUKLA <akashshukla94097@gmail.com>
2026-08-05 14:04:14 +0000

    variable

---
+++ basic/variable.js
@@ -0,0 +1,15 @@
+const name="akash"
+var email="akashshukla"
+let age=123
+section = "cse-12"
+//name="anuj"
+email="anuj@gogle"
+age=12
+section="cse23"
+
+console.log(name)
+console.log(age)
+console.log(section)
+console.log(email)
+console.table([name,age,section,email])
+console.log("akash")
