978ff1fe740c33c863c1298d7628793abe82b318
AKASH SHUKLA <akashshukla94097@gmail.com>
2026-08-06 12:57:36 +0000

    string

---
+++ basic/string.js
@@ -0,0 +1,9 @@
+const name="akash"
+const number=12
+console.log(`my name is ${name} and my number is ${number}`);
+const gane=new String('akash')
+console.log(gane[0]);
+console.log(gane.charAt(2));
+console.log(gane.indexOf('a'));
+const url = "https://www.youtube.com/watch?v=5qap5aO4i9A"
+console.log(url.replace('youtube','akash'));
