dd276772091f95b96572d41f37344bf8f62546a1
AKASH SHUKLA <akashshukla94097@gmail.com>
2026-08-08 13:00:58 +0000

    array

---
+++ basic2/array.js
@@ -0,0 +1,43 @@
+let arr=[1,2,3,4]
+
+// let arr2=new Array(1,2,3,4)
+// console.log(arr[2])
+// console.log(arr2[2])
+// arr.push(6)
+// arr.push(8)
+// arr.pop()
+// console.log(arr)
+// //start me add
+// arr.unshift(9)
+// console.log(arr)
+// //ange se hatane ke liye
+// arr.shift()
+// console.log(arr)
+// console.log(arr.indexOf(3))
+// console.log("A", arr)
+// let newarr=arr.slice(1,3)
+// console.log(newarr)
+// console.log("B", arr)
+// let newarr2=arr.splice(1,3)
+// console.log("C", arr)
+// console.log(newarr2)
+let arr1=[1,2,3,4]
+let arr2=[5,6,7,8]
+// arr1.push(arr2)
+// console.log(arr1)
+// console.log(arr1[4][3])
+
+// let arr3=arr1.concat(arr2)
+// console.log(arr3)
+console.log(Array.isArray("akash"))
+console.log(Array.from("akash"))
+let s1=2
+let s2=2
+let s3=2
+console.log(Array.of(s1,s2,s3))
+
+
+
+
+
+
