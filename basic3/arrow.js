5cd2922b3ddf1302f8ee0f1413e34dfafa4a2f1b
AKASH SHUKLA <akashshukla94097@gmail.com>
2026-08-10 14:25:55 +0000

    arrow

---
+++ basic3/arrow.js
@@ -0,0 +1,34 @@
+// const obj={
+//     name:"akash",
+//     age:45,
+//     welcome:function(){
+//         console.log(`${this.name} hello ji`);
+        
+//     }
+// }
+// obj.welcome()
+// obj.name="sam"
+// obj.welcome()
+// const chai=()=>{
+//     let name="akash"
+//     //this function me nahi chalta only obj
+//     console.log(this);
+    
+// }
+// chai()
+
+
+// method of writing arrow function
+// const add =(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(add(3,5));
+            //or
+// const add =(num1,num2)=> num1+num2
+
+// console.log(add(3,5));
+
+           //or
+const add =(num1,num2)=> (num1+num2)
+
+console.log(add(3,5));
