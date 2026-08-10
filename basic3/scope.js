9a9cc091068a5ca1e8c20d808b8b9d3a35948d75
AKASH SHUKLA <akashshukla94097@gmail.com>
2026-08-10 13:50:53 +0000

    scope

---
+++ basic3/scope.js
@@ -0,0 +1,10 @@
+var c=87
+
+if(true){
+    let a=3;
+    const b=4;
+    var c=5;//if we donot write anything then also work as a var ,c=5
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
